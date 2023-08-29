2449d50d160df48adad05cb025e8a3b4ad73767e
prajwal160 <alaladinniprajwal@gmail.com>
2023-08-29 12:12:37 +0530

    added packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import { Button, FormControl,InputLabel, Input } from '@mui/material';
 import './App.css';
+import Message from './Message';
+
 
 function App() {
+
+  const[input, setInput] = useState('');
+  const[messages, setMessages]= useState(["hello","hlo","bro"]);
+
+  const sendMessage = (event) =>{ 
+    event.preventDefault(); 
+    // all the logic to send a message goes
+    setMessages ([...messages, input]); 
+    setInput('');
+  }
+  console.log(input);
+  console.log(messages)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>Hlo India</h1>
+<form>
+
+<FormControl>
+  <InputLabel >Enter a message</InputLabel>
+  <Input  />
+  <Button disabled={!input} variant="contained" type="submit" onClick={sendMessage}>Send message</Button>
+</FormControl>
+
+{/* <input value ={input} onChange={event => setInput(event.target.value)}/> */}
+  {/* <Button disabled={!input} variant="contained" type="submit" onClick={sendMessage}>Send message</Button> */}
+</form>
+
+  {
+  messages.map(message => (
+    <Message text ={message}/>
+   // <p>{message}</p>
+  ))
+  }
+  
     </div>
   );
 }
